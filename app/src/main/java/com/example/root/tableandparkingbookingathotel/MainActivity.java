51d9c7dfdf9b22037be2be54c53c4242e537a0a7
root <patiltejas24@gmail.com>
2016-09-21 19:55:17 -0400

    added a botton on main activity

--- app/src/main/java/com/example/root/tableandparkingbookingathotel/MainActivity.java
+++ app/src/main/java/com/example/root/tableandparkingbookingathotel/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +23,19 @@ public class MainActivity extends AppCompatActivity {
                     {
                         Intent intent = new Intent(findViewById(R.id.SignIn).getContext(),HomePage.class);
                         startActivity(intent);
+                    }
+                }
+        );
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        TextView textView = (TextView) findViewById(R.id.button);
+                        textView.setText("wow");
+
                     }
                 }
         );
